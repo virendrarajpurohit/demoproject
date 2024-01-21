01893c97f376dabb8b4309751277ddb47de4c48b
virendrarajpurohit <37048535+virendrarajpurohit@users.noreply.github.com>
2024-01-21 13:24:45 +0530

    Update makeVideo.js

--- makeVideo.js
+++ makeVideo.js
@@ -201,7 +201,15 @@ function loop(i){
 
       function uploadVideoFile(){
       const video1 = { path: 'out.mp4', title: 'test video title', description: 'test video description' }
-       upload (credentials, [video1], {headless:true, args :["--disable-web-security","--no-sandbox", "--disabled-setuid-sandbox"]}).then(console.log)
+       upload (credentials, [video1], {headless:true, args :[ '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-setuid-sandbox',
+        '--no-first-run',
+        '--no-sandbox',
+        '--no-zygote',
+        '--deterministic-fetch',
+        '--disable-features=IsolateOrigins',
+        '--disable-site-isolation-trials',]}).then(console.log)
       }
 
 
