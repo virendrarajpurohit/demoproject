fb4caac06bad03e911f0fabb9d7798f2ed384630
virendra raj <37048535+virendrarajpurohit@users.noreply.github.com>
2024-01-26 15:21:45 +0530

    Update makeVideo.js

--- makeVideo.js
+++ makeVideo.js
@@ -1,11 +1,11 @@
-var ffmpeg = require('fluent-ffmpeg');
-var axios = require('axios');
+//var ffmpeg = require('fluent-ffmpeg');
+//var axios = require('axios');
 const fs = require('fs');
-const riddles = require('./riddles.js');
+//const riddles = require('./riddles.js');
 const { upload } = require('youtube-uploader'); //vanilla javascript
 const { exec } = require('child_process');
 
-const inputVideo = 'out.mp4';
+//const inputVideo = 'out.mp4';
 //const outputVideo = 'zw1.mp4';
 
 // recoveryemail is optional, only required to bypass login with recovery email if prompted for confirmation
@@ -39,20 +39,20 @@ const onVideoUploadSuccess = (videoUrl) => {
 //   }, (2 * 60 * 1000)*i)
 // }
 
-(async  () => {
-  for (let i = 0; i < 1; i++) {
+// (async  () => {
+//   for (let i = 0; i < 1; i++) {
           
-          await delay();
-          console.log(i);
-          loop(i);
-  }
-})();
+//           await delay();
+      //    console.log(i);
+       //   loop(i);
+//   }
+// })();
 
-function delay() {
-  return new Promise((resolve, reject) => {
-          setTimeout(resolve, 30000);
-  });
-}
+// function delay() {
+//   return new Promise((resolve, reject) => {
+//           setTimeout(resolve, 30000);
+//   });
+// }
 
 // for (let i = 0; i < riddles.length; i++)
 //   (function(i) {
@@ -104,7 +104,7 @@ function delay() {
 
 // }
 
-function loop(i){
+// function loop(i){
 
     //setTimeout(generateTextFile, 3000, i);
         
@@ -112,102 +112,102 @@ function loop(i){
 
     //setTimeout(applyEffectToVideo, 20000, i);
   
-    setTimeout(uploadVideoFile, 5000);
+    //setTimeout(uploadVideoFile, 5000);
   
-}
+//}
 
        
        
-       function generateTextFile(i) {
-        var data = riddles[i];
-        data = data.split(/((?:\S+\s*){5})/g).filter(Boolean).join('\n\n');
+    //    function generateTextFile(i) {
+    //     var data = riddles[i];
+    //     data = data.split(/((?:\S+\s*){5})/g).filter(Boolean).join('\n\n');
         
-         fs.writeFile('riddle.txt', data, (err) => {
-          if (err) {
-            throw err;
-          }
-          console.log('The file has been saved!');
+    //      fs.writeFile('riddle.txt', data, (err) => {
+    //       if (err) {
+    //         throw err;
+    //       }
+    //       console.log('The file has been saved!');
           
-        });
-      }
+    //     });
+    //   }
 
-       function generateVideoFile(){
-        var proc = ffmpeg('./Virendra.mp4').duration(20)
-        .videoFilters({ 
-      filter: 'drawtext',
-      options: {
-        fontfile:'sans-serif.ttf',
-        textfile: 'riddle.txt',
-        fontsize: 50,
-        fontcolor: 'black',
-        x:'(w-text_w)/2',
-        y:'(h-text_h)/2',
-        shadowcolor: 'white',
-        shadowx: 0,
-        enable:"between(t,0,10)",
-        shadowy: 0,
-      } 
-    })
-    .addInput('./joyful-snowman_60sec-176773.mp3')
-    // .videoFilters({
-    //     filter: 'drawtext',
-    //     options: {
-    //       fontfile:'font.ttf',
-    //       text: 'can change your whole day' ,
-    //       fontsize: 20,
-    //       fontcolor: 'white',
-    //       x: '(main_w/2-text_w/2)',
-    //       y: 50,
-    //       shadowcolor: 'black',
-    //       shadowx: 2,
-    //       enable:"between(t,6,10)",
-    //       shadowy: 2
-    //     }
+    //    function generateVideoFile(){
+    //     var proc = ffmpeg('./Virendra.mp4').duration(20)
+    //     .videoFilters({ 
+    //   filter: 'drawtext',
+    //   options: {
+    //     fontfile:'sans-serif.ttf',
+    //     textfile: 'riddle.txt',
+    //     fontsize: 50,
+    //     fontcolor: 'black',
+    //     x:'(w-text_w)/2',
+    //     y:'(h-text_h)/2',
+    //     shadowcolor: 'white',
+    //     shadowx: 0,
+    //     enable:"between(t,0,10)",
+    //     shadowy: 0,
+    //   } 
     // })
-      //  .input('Video2.mp4')
-        .on('end', function () {
-            console.log('file has been converted succesfully');
-        })
-        .on('error', function (err) {
-            console.log('an error happened: ' + err.message);
-        })
-        // save to file
-       .save('./out.mp4');
+    // .addInput('./joyful-snowman_60sec-176773.mp3')
+    // // .videoFilters({
+    // //     filter: 'drawtext',
+    // //     options: {
+    // //       fontfile:'font.ttf',
+    // //       text: 'can change your whole day' ,
+    // //       fontsize: 20,
+    // //       fontcolor: 'white',
+    // //       x: '(main_w/2-text_w/2)',
+    // //       y: 50,
+    // //       shadowcolor: 'black',
+    // //       shadowx: 2,
+    // //       enable:"between(t,6,10)",
+    // //       shadowy: 2
+    // //     }
+    // // })
+    //   //  .input('Video2.mp4')
+    //     .on('end', function () {
+    //         console.log('file has been converted succesfully');
+    //     })
+    //     .on('error', function (err) {
+    //         console.log('an error happened: ' + err.message);
+    //     })
+    //     // save to file
+    //    .save('./out.mp4');
 
-        // ffmpeg()
-        // .addInput('./Akhand Sarcastic.mp4') //your video file input path
-        // .addInput('./joyful-snowman_60sec-176773.mp3') //your audio file input path
-        // .output('/.outp.mp4') //your output path
-        // .outputOptions(['-map 0:v', '-map 1:a', '-c:v copy', '-shortest'])
-        // .save('/.outp.mp4')
-      }
+    //     // ffmpeg()
+    //     // .addInput('./Akhand Sarcastic.mp4') //your video file input path
+    //     // .addInput('./joyful-snowman_60sec-176773.mp3') //your audio file input path
+    //     // .output('/.outp.mp4') //your output path
+    //     // .outputOptions(['-map 0:v', '-map 1:a', '-c:v copy', '-shortest'])
+    //     // .save('/.outp.mp4')
+    //   }
 
-      function applyEffectToVideo(i) {
-        const filterExpression = `[0][1]scale2ref,overlay='if(gt(random(0), 0.2), 1, 4)':'if(gt(random(0), 0.1), 1, 2)', "boxblur=1" `;
-        //const filterExpression = `[0][1]scale2ref,overlay=x='if(gt(random(0), 0.2), 1, 4)':'if(gt(random(0), 0.1), 1, 2)', "boxblur=1" `;
-        const ffmpegCommand = `ffmpeg -f lavfi -i color=s=1080x1920 -i ${inputVideo} -filter_complex "${filterExpression}" -t 20 zw${i}.mp4`;
-        exec(ffmpegCommand, (error, stdout, stderr) => {
-          if (error) {
-            console.error(`Error: ${error.message}`);
-            return;
-          }
-          if (stderr) {
-            console.error(`FFmpeg stderr: ${stderr}`);
-            return;
-          }
-          console.log('Shake effect applied successfully!');
-        });
-      }
+      // function applyEffectToVideo(i) {
+      //   const filterExpression = `[0][1]scale2ref,overlay='if(gt(random(0), 0.2), 1, 4)':'if(gt(random(0), 0.1), 1, 2)', "boxblur=1" `;
+      //   //const filterExpression = `[0][1]scale2ref,overlay=x='if(gt(random(0), 0.2), 1, 4)':'if(gt(random(0), 0.1), 1, 2)', "boxblur=1" `;
+      //   const ffmpegCommand = `ffmpeg -f lavfi -i color=s=1080x1920 -i ${inputVideo} -filter_complex "${filterExpression}" -t 20 zw${i}.mp4`;
+      //   exec(ffmpegCommand, (error, stdout, stderr) => {
+      //     if (error) {
+      //       console.error(`Error: ${error.message}`);
+      //       return;
+      //     }
+      //     if (stderr) {
+      //       console.error(`FFmpeg stderr: ${stderr}`);
+      //       return;
+      //     }
+      //     console.log('Shake effect applied successfully!');
+      //   });
+      // }
 
-      function uploadVideoFile(){
-      const video1 = { path: 'out.mp4', title: 'test video title', description: 'test video description' }
+      //function uploadVideoFile(){
+      const video1 = { path: 'out.mp4', title: 'title', description: 'description' }
        upload (credentials, [video1], {headless:false, args :[ '--disable-gpu',
         '--disable-dev-shm-usage',
         '--disable-setuid-sandbox',
         '--no-sandbox',
         '--no-zygote',
         "enable-automation","disable-web-security","allow-running-insecure-content","--start-maximized"]}).then(console.log)
-      }
+      //}
 
 
 
