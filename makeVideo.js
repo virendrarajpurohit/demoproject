ec76457576096599968f9371ac352dd820e1072f
virendra raj <37048535+virendrarajpurohit@users.noreply.github.com>
2024-01-27 20:45:37 +0530

    Update makeVideo.js

--- makeVideo.js
+++ makeVideo.js
@@ -204,7 +204,8 @@ const onVideoUploadSuccess = (videoUrl) => {
 
  const video1 = { path: 'out.mp4', title: 'title hai', description: 'description' }
        upload (credentials, [video1], {headless:true, ignoreHTTPSErrors: true, defaultViewport: null, ignoreDefaultArgs: ["--enable-automation"], args :['--no-sandbox','--disable-web-security',
-         '--start-fullscreen', '--disable-infobars', '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"']}).then(console.log)
+         '--start-fullscreen', '--disable-infobars', '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36
+"']}).then(console.log)
 
  
 
